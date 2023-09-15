676830f50544384b9417b5c5ba688098cdc067e7
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-15 12:04:56 -0500

    Initial commit client

---
+++ src/main/java/com/jp/configclient/JpConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.jp.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/configclient/JpConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
