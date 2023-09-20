59063353e85c8092694deb5efa717445f180a2a9
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-20 15:45:47 -0500

    Add prometheus configuration in the ms

--- src/main/java/com/jp/configclient/controller/AppController.java
+++ src/main/java/com/jp/configclient/controller/AppController.java
@@ -2,6 +2,7 @@ package com.jp.configclient.controller;
 
 import com.jp.configclient.config.Config;
 import com.jp.configclient.service.CustomLogService;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/application-name")
 public class AppController {
 
     @Autowired
     private CustomLogService customLogService;
+
+    /**
+     * aquí injectamos el bean
+     */
+    @Autowired
+    private MeterRegistry meterRegistry;
     private final Config config;
 
     @Value("${server.port}")
@@ -26,6 +35,13 @@ public class AppController {
 
     @GetMapping
     public ResponseEntity<String> getAppName() {
+        // esto sirve para crear una nueva métrica personalizada, esta métrica
+        // solo aparecerá una vez se llame este método
+//        meterRegistry.counter("jp.com.configclient.name").increment();
+//        ahora lo que hago aquí es que la métrica tenga un tag, segun el valor
+        int value = new Random().nextInt();
+        meterRegistry.counter("jp.com.configclient.metric.name", "level",
+                (value < 3 ? "jr" : "sr")).increment(value);
         customLogService.printLog();
         return ResponseEntity.ok(config.getApplicationName() + "My port is: " + serverPort);
     }
